bbcfd4cd1ebd2c9f1d2cba73c404cb872fb2cacc
AtaaNkpa20 <rquaye023@st.ug.edu.gh>
2025-08-04 15:19:45 +0100

    Completed Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Interfaces
+{
+    // Interface definition
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    // Class implementing IMovable
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Another class implementing IMovable
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Interfaces Demo ===");
+            Console.WriteLine();
+
+            // Create instances of Car and Bicycle
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            // Call Move() method on each instance
+            car.Move();
+            bicycle.Move();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
