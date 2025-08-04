042cdb3f44c2302993d32fbe02d62238d95af932
AtaaNkpa20 <rquaye023@st.ug.edu.gh>
2025-08-04 15:09:42 +0100

    Completed Inhenritance and Method Overriding

---
+++ Inheritance/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+    
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Inheritance and Method Overriding Demo ===");
+            Console.WriteLine();
+
+            // Create instances of each class
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Call MakeSound() method on each instance
+            Console.Write("Animal sound: ");
+            animal.MakeSound();
+
+            Console.Write("Dog sound: ");
+            dog.MakeSound();
+
+            Console.Write("Cat sound: ");
+            cat.MakeSound();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
